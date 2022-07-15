2879d93ec9c0c06a677371263dfc0679094b4f63
Kamil Shaikha <95491864+KamilSh98@users.noreply.github.com>
2022-07-15 14:49:27 +0200

    Add files via upload

--- circle.js
+++ circle.js
@@ -1,12 +1,108 @@
-let number = document.getElementById("number");
-let counter = 0;
+let numberHTML = document.getElementById("numberHTML");
+let counterHTML = 0;
         
         setInterval(() => {
-            if(counter == 65){
+            if(counterHTML == 85){
                 clearInterval();
             }
             else{
-                counter += 1;
-                number.innerHTML = counter + "%";
+                counterHTML += 1;
+                numberHTML.innerHTML = counterHTML + "%";
+            }
+        }, 20)
+
+
+let numberCSS = document.getElementById("numberCSS");
+let counterCSS = 0;
+        
+        setInterval(() => {
+            if(counterCSS == 80){
+                clearInterval();
+            }
+            else{
+                counterCSS += 1;
+                numberCSS.innerHTML = counterCSS + "%";
+            }
+        }, 20)
+
+
+let numberJS = document.getElementById("numberJS");
+let counterJS = 0;
+        
+        setInterval(() => {
+            if(counterJS == 50){
+                clearInterval();
+            }
+            else{
+                counterJS += 1;
+                numberJS.innerHTML = counterJS + "%";
+            }
+        }, 20)
+
+
+let numberBootstrap = document.getElementById("numberBootstrap");
+let counterBootstrap = 0;
+        
+        setInterval(() => {
+            if(counterBootstrap == 60){
+                clearInterval();
+            }
+            else{
+                counterBootstrap += 1;
+                numberBootstrap.innerHTML = counterBootstrap + "%";
+            }
+        }, 20)
+
+
+let numberPHP = document.getElementById("numberPHP");
+let counterPHP = 0;
+        
+        setInterval(() => {
+            if(counterPHP == 15){
+                clearInterval();
+            }
+            else{
+                counterPHP += 1;
+                numberPHP.innerHTML = counterPHP + "%";
+            }
+        }, 20)
+
+
+let numberMYSQL = document.getElementById("numberMYSQL");
+let counterMYSQL = 0;
+        
+        setInterval(() => {
+            if(counterMYSQL == 20){
+                clearInterval();
+            }
+            else{
+                counterMYSQL += 1;
+                numberMYSQL.innerHTML = counterMYSQL + "%";
+            }
+        }, 20)
+
+let numberWordpress = document.getElementById("numberWordpress");
+let counterWordpress = 0;
+        
+        setInterval(() => {
+            if(counterWordpress == 35){
+                clearInterval();
+            }
+            else{
+                counterWordpress += 1;
+                numberWordpress.innerHTML = counterWordpress + "%";
+            }
+        }, 20)
+
+let numberJoomla = document.getElementById("numberJoomla");
+let counterJoomla = 0;
+        
+        setInterval(() => {
+            if(counterJoomla == 30){
+                clearInterval();
+            }
+            else{
+                counterJoomla += 1;
+                numberJoomla.innerHTML = counterJoomla + "%";
             }
         }, 20)
