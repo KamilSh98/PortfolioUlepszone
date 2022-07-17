30e589ca6e684f118eee55dda567de5cfe7e2c19
Kamil Shaikha <95491864+KamilSh98@users.noreply.github.com>
2022-07-17 22:57:34 +0200

    Add files via upload

--- spinner.js
+++ spinner.js
@@ -1,5 +1,11 @@
 let spinnerWrapper = document.querySelector('.spinner-wrapper');
     
         window.addEventListener('load', function () {
+            setTimeout(function(){
             spinnerWrapper.parentElement.removeChild(spinnerWrapper);
+            document.body.style.overflow = "auto"
+            }, 2000)
         });
+
+
+        
