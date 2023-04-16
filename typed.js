17117302d784e2d6d74bb5cd8ab2c36e726e4653
Kamil Shaikha <95491864+KamilSh98@users.noreply.github.com>
2023-04-16 22:31:05 +0200

    Add files via upload

--- typed.js
+++ typed.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".animatedText",{
-    strings: ["Nazywam się Kamil Shaikha"],
+    strings: ["Nazywam się Kamil Shaikha","Pragnę zostać front-end developerem"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
