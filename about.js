1e338cd34e3a26ce91f6b386984d057e78ad1d39
Kamil Shaikha <95491864+KamilSh98@users.noreply.github.com>
2023-04-20 18:44:10 +0200

    Add files via upload

--- about.js
+++ about.js
@@ -6,20 +6,33 @@ var educationMore = document.getElementById("education-more");
 var coursesMore = document.getElementById("courses-more");
 var thesisMore = document.getElementById("thesis-more");
 
+var educationText = document.getElementById("education-text");
+var coursesText = document.getElementById("courses-text");
+var thesisText = document.getElementById("thesis-text");
+
 education.addEventListener('click', function () {
     educationMore.style.display = "grid";
     coursesMore.style.display = "none";
     thesisMore.style.display = "none";
+    educationText.style.color = "#378f95";
+    coursesText.style.color = "#fff";
+    thesisText.style.color = "#fff";
 });
 
 courses.addEventListener('click', function () {
     educationMore.style.display = "none";
     coursesMore.style.display = "grid";
     thesisMore.style.display = "none";
+    educationText.style.color = "#fff";
+    coursesText.style.color = "#378f95";
+    thesisText.style.color = "#fff";
 });
 
 thesis.addEventListener('click', function () {
     educationMore.style.display = "none";
     coursesMore.style.display = "none";
     thesisMore.style.display = "grid";
+    educationText.style.color = "#fff";
+    coursesText.style.color = "#fff";
+    thesisText.style.color = "#378f95";
 });
